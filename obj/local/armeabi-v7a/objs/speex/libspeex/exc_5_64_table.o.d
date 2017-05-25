2d52dcaaa71f248a6e784aa58d85bdc223452b36
weiersyuan <775700335@qq.com>
2017-05-25 19:10:21 +0800

    删除obj目录下的文件

--- obj/local/armeabi-v7a/objs/speex/libspeex/exc_5_64_table.o.d
+++
@@ -1,2 +0,0 @@
-obj/local/armeabi-v7a/objs/speex/libspeex/exc_5_64_table.o: \
- jni/libspeex/exc_5_64_table.c
--- obj/local/armeabi-v7a/objs/speex/libspeex/hexc_table.o.d
+++
@@ -1,2 +0,0 @@
-obj/local/armeabi-v7a/objs/speex/libspeex/hexc_table.o: \
- jni/libspeex/hexc_table.c
